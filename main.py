020cae92a6e8cf3c34303c47162714da097cec2c
Arthur Valingot <arthur@workera.ai>
2021-10-29 11:52:51 +0200

    add dijkstra algorithm feature

--- main.py
+++ main.py
@@ -75,7 +75,27 @@ def fetch_data(conn, token):
                 href = link['href']
                 break
 
+def dijkstra(self, start_vertex):
+    D = {v:float('inf') for v in range(self.v)}
+    D[start_vertex] = 0
 
+    pq = PriorityQueue()
+    pq.put((0, start_vertex))
+
+    while not pq.empty():
+        (dist, current_vertex) = pq.get()
+        self.visited.append(current_vertex)
+
+        for neighbor in range(self.v):
+            if self.edges[current_vertex][neighbor] != -1:
+                distance = self.edges[current_vertex][neighbor]
+                if neighbor not in self.visited:
+                    old_cost = D[neighbor]
+                    new_cost = D[current_vertex] + distance
+                    if new_cost < old_cost:
+                        pq.put((new_cost, neighbor))
+                        D[neighbor] = new_cost
+    return D
 
 if __name__ == "__main__":
     client_id = ''
