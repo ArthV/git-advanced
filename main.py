9e91d728c9f43054c6f84b1d52e61e003944ce8c
Arthur Valingot <arthur@workera.ai>
2021-10-29 11:57:47 +0200

    Add baby step, and giant step

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import http.client
 from os import access
 import ast
 import csv
-from queue import PriorityQueue
-
+from  math import ceil, sqrt
 
 def initiate_connection():
     conn = http.client.HTTPSConnection('api.linkedin.com')
@@ -96,6 +95,27 @@ def dijkstra(self, start_vertex):
                         pq.put((new_cost, neighbor))
                         D[neighbor] = new_cost
     return D
+def bsgs(g, h, p):
+    '''
+    Solve for x in h = g^x mod p given a prime p.
+    If p is not prime, you shouldn't use BSGS anyway.
+    '''
+    N = ceil(sqrt(p - 1))  # phi(p) is p-1 if p is prime
+
+    # Store hashmap of g^{1...m} (mod p). Baby step.
+    tbl = {pow(g, i, p): i for i in range(N)}
+
+    # Precompute via Fermat's Little Theorem
+    c = pow(g, N * (p - 2), p)
+
+    # Search for an equivalence in the table. Giant step.
+    for j in range(N):
+        y = (h * pow(c, j, p)) % p
+        if y in tbl:
+            return j * N + tbl[y]
+
+    # Solution not found
+    return None
 
 if __name__ == "__main__":
     client_id = ''
